a2eafb5f8bbed5043604380c7e40bb164563c618
RahelRegina <pushparegina0123@gmail.com>
2024-09-03 15:43:19 +0530

    new commit

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "./assets/image";
 import './MainPage.css';
 const MainPage=()=>{
+
+    const [copyMessage,setCopyMessage]=useState('');
+
+
+    const copyToClipboard = (text, type) => {
+        navigator.clipboard.writeText(text).then(() => {
+            setCopyMessage(`${type} copied`);
+            setTimeout(() => setCopyMessage(""), 2000); // Hide message after 2 seconds
+        });
+    };
     return(
         <div>
             <header>
@@ -12,39 +22,40 @@ const MainPage=()=>{
                     </div>
                     <div className="bars">
                         <div className="home">
-                            <a href="home">HOME</a>
+                            <a href="#home">HOME</a>
                         </div>
                         <div className="about">
-                            <a href="abt">ABOUT</a>
+                            <a href="#about">ABOUT</a>
                         </div>
                         <div className="skill">
-                            <a href="skill">SKILLS</a>
+                            <a href="#skills">SKILLS</a>
                         </div>
                         <div className="project">
-                            <a href="project">PROJECTS</a>
+                            <a href="#projects">PROJECTS</a>
                         </div>
                         <div className="education">
-                            <a href="education">EDUCATION</a>
+                            <a href="#education">EDUCATION</a>
                         </div>
                         <div className="contact">
-                            <a href="contact">CONTACT</a>
+                            <a href="#contact">CONTACT</a>
                         </div>
                     </div>
 
                 </div>
             </header>
-            <section className="home-context">
+            <section id="home" className="home-context">
                 <div className="text">
                     <h1><span>Hi There,<br/></span>I'm Pushpa Regina</h1>
                     <h2>I'm into FullStack Development</h2>
+                    <a href="/Pushparegina S.pdf" download>
                     <button>Resume<img src={assets.download_icon} alt="dwld"/></button>
-                    
+                    </a>
                 </div>
                 <div className="avatar">
                     <img src={assets.avatar_icon} alt="avatar"/>
                 </div>
             </section>
-            <section className="about-me">
+            <section id="about" className="about-me">
                 <div className="content">
                     <h2>About Me</h2>
                     <ul>
@@ -57,7 +68,7 @@ const MainPage=()=>{
                 </div>
             </section>
             <h2 className="title">SKILLS</h2>
-            <section className="myskills">
+            <section id="skills" className="myskills">
                 <div className="skills-container">
                     {/* <h2>SKILLS</h2> */}
                     <div className="html">
@@ -104,32 +115,32 @@ const MainPage=()=>{
                 </div>
             </section>
             <h2 className="work">PROJECTS</h2>
-            <section className="projects">
+            <section id="projects" className="projects">
                 <div className="my-project">
                     <div className="pro1">
                         <span>Blog Website</span>
-                        <a href="blog">https://github.com/reginaragel/Blog-Website/tree/master</a>
+                        <a href="https://github.com/reginaragel/Blog-Website/tree/master">View on Github</a>
                     </div>
                     <div className="pro2">
                         <span>Book Website</span>
-                        <a href="book">https://github.com/reginaragel/Book/tree/master</a>
+                        <a href="https://github.com/reginaragel/Book/tree/master">View on Github</a>
                     </div>
                     <div className="pro3">
                         <span>Flipkart Clone FrontEnd</span>
-                        <a href="flip">https://github.com/reginaragel/ProfileFyiProject/tree/master</a>
+                        <a href="https://github.com/reginaragel/ProfileFyiProject/tree/master">View on Github</a>
                     </div>
                     <div className="pro4">
                         <span>Task Manager</span>
-                        <a href="task">https://github.com/reginaragel/TaskManager_Gamerback/tree/master</a>
+                        <a href="https://github.com/reginaragel/TaskManager_Gamerback/tree/master">View on Github</a>
                     </div>
                     <div className="pro5">
                         <span>TodoList</span>
-                        <a href="todo">https://github.com/reginaragel/Todolist/tree/master</a>
+                        <a href="https://github.com/reginaragel/Todolist/tree/master">View on Github</a>
                     </div>
                 </div>
             </section>
             <h2 className="study">EDUCATION</h2>
-            <section className="my-edu">
+            <section id="education" className="my-edu">
                 <div className="degree">
                     <img src={assets.wcc_icon} alt="wcc"/>
                     <p>Women's Christian College</p>
@@ -150,27 +161,28 @@ const MainPage=()=>{
                 </div>
             </section>
             <h2 className="call">CONTACT DETAILS</h2>
-            <section className="my-contact">
+            <section id="contact" className="my-contact">
                 <div className="contact-me">
                     <div className="link">
-                        <img src={assets.linkedin_icon} alt="linkedin"/>
+                       <a href="https://www.linkedin.com/in/pushparegina/"> <img src={assets.linkedin_icon} alt="linkedin"/></a>
                     </div>
                     <div className="gits">
-                        <img src={assets.github_icon} alt="github"/>
+                       <a href="https://github.com/reginaragel"> <img src={assets.github_icon} alt="github"/></a>
                     </div>
                     <div className="twitter">
-                        <img src={assets.twitter_icon} alt="twitter"/>
+                        <a href="https://x.com/Regina92865760"><img src={assets.twitter_icon} alt="twitter"/></a>
                     </div>
                     <div className="insta">
-                        <img src={assets.insta_icon} alt="insta"/>
+                        <a href="https://www.instagram.com/raghel_regina/"><img src={assets.insta_icon} alt="insta"/></a>
                     </div>
-                    <div className="email">
+                    <div className="email" onClick={()=>copyToClipboard('pushparegina0123@gmail.com','Mail')}>
                         <img src={assets.mail_icon} alt="mail"/>
                     </div>
-                    <div className="phone">
+                    <div className="phone" onClick={()=>copyToClipboard('9342138662','Number')}>
                         <img src={assets.phone_icon} alt="phone"/>
                     </div>
                 </div>
+                {copyMessage && <div className="copy-message">{copyMessage}</div>}
             </section>
 
         </div>
